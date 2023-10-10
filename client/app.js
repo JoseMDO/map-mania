fb211ba128f8cf92f23beee9fe886e04aa3b2399
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-10 02:22:29 -0500

    mapped all favorite locations to map

--- client/app.js
+++ client/app.js
@@ -1,25 +1,36 @@
 let gMap;
+let locationsArray;
 
 async function initMap() {
     try{
         const response = await fetch('https://map-mania-json.azurewebsites.net/api/favorite-places');
         const result  = await response.json();
-        console.log(result);
+        locationsArray = result;
     } catch(err) {
         console.log(err)
     }
 
     //@ts-ignore
     const { Map } = await google.maps.importLibrary("maps");
-  
-    // The map, centered at Uluru
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
     gMap = new Map(document.getElementById("myMapID"), {
       zoom: 4,
       center: {lat: 41.5250, lng: -88.0817},
       mapId: "DEMO_MAP_ID",
     });
   
-    // The marker, positioned at Uluru
+    function mapLocations(locationsArray) {
+        locationsArray.forEach((location) => {
+            const marker = new google.maps.Marker({
+                position: {lat:location.location.lat, lng:location.location.lng},
+                map: gMap,
+                title: location.title,
+                icon: location.iconImage
+            })
+        });
+    }
+  
 
     // const marker2 = new google.maps.Marker({
     //     map: gMap,
@@ -50,7 +61,8 @@ async function initMap() {
         }
         console.log("inbounds:" + inBounds + " xoomLevel: "+zoomLevel);
     }
+    mapLocations(locationsArray);
   }
   
-  initMap();
+initMap();
   
